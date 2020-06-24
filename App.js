c0b07cc98cd07fed2cb677ba24e6d9dca6ad12e8
Paulo Cardoso <paulo_jcs@yahoo.com.br>
2020-06-24 14:33:59 -0300

    Exercicio React-native

--- App.js
+++ App.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Component} from 'react';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+import { HeaderBackground } from 'react-navigation-stack';
+import Padrao from './src/estilo/padrao';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+class ArcoIris extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      cor_jecasi : [
+        Padrao.estilo_cor1,
+        Padrao.estilo_cor2,
+        Padrao.estilo_cor3,
+        Padrao.estilo_cor4,
+        Padrao.estilo_cor5,
+
+      ], 
+    };
+  }
+
+  componentDidMount(){
+    setInterval(() => {
+
+      const tirarUltimaCor= this.state.cor_jecasi.slice(-1);
+      const listaSemAUltimaCor = this.state.cor_jecasi.slice(0, -1); 
+
+
+
+      this.setState({cor_jecasi: tirarUltimaCor.concat(listaSemAUltimaCor) })
+
+    },1000)
+  }
+
+  render(){
+    /*
+    const cor_jecasi = [
+      Padrao.estilo_cor1,
+      Padrao.estilo_cor2,
+      Padrao.estilo_cor3,
+      Padrao.estilo_cor4,
+      Padrao.estilo_cor5,
+
+    ];
+    const views_jecasi = cor_jecasi.map(varias_cores => (
+    */
+    const views_jecasi = this.state.cor_jecasi.map(varias_cores => (
+
+        <View key={varias_cores} style={varias_cores}>  
+        </View>
+
+      ));
+    return(   
+      <View style = {Padrao.estilo_flex1}>
+        {views_jecasi}
+      </View>
+      
+    );
+  }
+}    
+export default ArcoIris;
---
+++ src/estilo/padrao.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native'
+export default StyleSheet.create({
+    estilo_flex1: {
+		flex: 1, 
+
+    },
+	estilo_cor1: {
+		flex: 1, 
+		backgroundColor: 'green',
+
+	},
+	estilo_cor2: {
+		flex: 1, 
+		backgroundColor: 'yellow',
+
+    },
+    estilo_cor3: {
+		flex: 1, 
+		backgroundColor: 'red',
+
+    },
+    estilo_cor4: {
+		flex: 1, 
+		backgroundColor: 'pink',
+
+    },
+    estilo_cor5: {
+		flex: 1, 
+		backgroundColor: 'blue',
+
+    },
+
+})
